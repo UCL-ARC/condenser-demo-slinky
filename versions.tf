36587a77155858ba7600d67f668c79388a031ba0
Camilla Harris <31481972+cdkharris@users.noreply.github.com>
2026-09-17 15:48:43 +0100

    Initial commit

---
+++ versions.tf
@@ -0,0 +1,16 @@
+# Enforce minimum Terraform and provider version numbers.
+terraform {
+  required_providers {
+    #aws = {
+    #  source  = "hashicorp/aws"
+    #  version = "~> 4.45.0"
+    #}
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.9.0"
+    }
+  }
+
+  required_version = ">= 1.8.5"
+}
