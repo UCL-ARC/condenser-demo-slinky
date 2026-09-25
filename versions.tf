050593c643c6b217d2c9c1cc0cfe9d3d106da3f1
Camilla Harris <camilla.harris@ucl.ac.uk>
2026-09-25 12:12:04 +0100

    add req'd versions

--- versions.tf
+++ versions.tf
@@ -1,16 +1,14 @@
-# Enforce minimum Terraform and provider version numbers.
 terraform {
   required_providers {
-    #aws = {
-    #  source  = "hashicorp/aws"
-    #  version = "~> 4.45.0"
-    #}
 
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.9.0"
+    harvester = {
+      source  = "harvester/harvester"
+      version = "~> 1.7.0"
     }
   }
 
   required_version = ">= 1.8.5"
+}
+
+provider "harvester" {
 }
